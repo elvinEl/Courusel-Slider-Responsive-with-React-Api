77114f8de8bfb1366bc76a4d1f43815793628bef
Yuzbekov Elvin <yuzbekov79@gmail.com>
2023-06-09 11:56:36 +0400

    React Api Slider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.min.css";
+import SwiperCore, { Autoplay, Pagination, Navigation } from "swiper";
 
-function App() {
+SwiperCore.use([Autoplay, Pagination, Navigation]);
+
+const App = () => {
+  const [slideData, setSlideData] = useState([]);
+  const [swiper, setSwiper] = useState(null);
+  const swiperRef = useRef(null);
+  const [slidesPerView, setSlidesPerView] = useState(3);
+  useEffect(() => {
+    const fetchSlideData = async () => {
+      try {
+        const response = await fetch("http://10.138.1.35:8000/api/v1/partners");
+        const data = await response.json();
+        setSlideData(data);
+      } catch (error) {
+        console.log("API çağrısı sırasında bir hata oluştu:", error);
+      }
+    };
+
+    fetchSlideData();
+  }, []);
+
+  useEffect(() => {
+    const updateSlidesPerView = () => {
+      if (window.innerWidth >= 1024) {
+        setSlidesPerView(3);
+      } else if (window.innerWidth >= 768) {
+        setSlidesPerView(2);
+      } else {
+        setSlidesPerView(1);
+      }
+    };
+
+    window.addEventListener("resize", updateSlidesPerView);
+
+    return () => {
+      window.removeEventListener("resize", updateSlidesPerView);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (swiper) {
+      swiper.autoplay.start();
+    }
+  }, [swiper]);
+
+  const handleSwiperInit = (swiperInstance) => {
+    setSwiper(swiperInstance);
+    swiperRef.current = swiperInstance;
+  };
+
+  const handleSlideChange = () => {
+    if (swiperRef.current && swiperRef.current.isEnd) {
+      // Slaytın sonuna gelindiğinde, slideData dizisini sonsuz döngüye sokmak için yeniden düzenliyoruz
+      setSlideData((prevData) => [...prevData.slice(1), prevData[0]]);
+    }
+  };
+
+  const handleSlideClick = () => {
+    if (swiperRef.current) {
+      swiperRef.current.autoplay.start();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-image-comments mt-20">
+      <br />
+      <div className="max-w-[80%] mx-auto pb-8">
+        {slideData.length > 0 ? (
+          <Swiper
+            spaceBetween={30}
+            slidesPerView={slidesPerView}
+            loop={true}
+            autoplay={{ delay: 5000, disableOnInteraction: false }}
+            navigation
+            pagination={{ clickable: true }}
+            onSwiper={handleSwiperInit}
+            onSlideChange={handleSlideChange}
+          >
+            {slideData.map((slide, index) => (
+              <SwiperSlide key={index} onClick={handleSlideClick}>
+                <div className="flex justify-center items-center bg-white w-full gap-8 bg-contain h-[200px] ">
+               
+                  {/* <p>{slide.name}</p> */}
+                  <img className="w-full"  src={slide.image} alt="" />
+                </div>
+              </SwiperSlide>
+            ))}
+          
+          </Swiper>
+        ) : (
+          <p>Veriler yükleniyor...</p>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
